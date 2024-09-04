d3db39436e4840b370dee46d2cca152e5d8aef55
hariharan-vn <hariharanvnseven@gmail.com>
2024-09-04 07:19:11 +0530

    Init Commit
    Committing the project after modifying git ignore files

---
+++ MultiplayerChallenge/Source/MultiplayerChallenge/MultiplayerChallenge.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MultiplayerChallenge : ModuleRules
+{
+	public MultiplayerChallenge(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ MultiplayerChallenge/Source/MultiplayerChallenge/MultiplayerChallengeGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MultiplayerChallengeGameMode.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMultiplayerChallengeGameMode::AMultiplayerChallengeGameMode()
+{
+}
